eefb6e0d0f2cfe48a99a5d816c8a5352f0fb505f
AnnaBezverkhnia <bezverkhniaa1@gmail.com>
2021-03-16 19:27:27 +0100

    Button Bold

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 // tady je místo pro náš program
+
+let nadpis = document.querySelector("h1");
+nadpis.classList.add("zluty");
+nadpis.classList.remove("zluty");
+nadpis.classList.toggle("zluty");
+
+function secti(a,b) {
+    console.log(a + b)
+}
+
+function ztucni() {
+    document.querySelector(".odstavec").style.fontWeight = "bold";
+}
